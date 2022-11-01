998406c7fbf41df43d93f272113c0bb6eea61afa
filip2cz <filip@fkomarek.eu>
2022-11-01 12:49:50 +0100

    export to csv + not saving passwd in file anymore

--- graph.ps1
+++ graph.ps1
@@ -5,10 +5,9 @@ $use_ssh = $false #if false, use ftp to transfer files from server
 $ask_for_path = $false #ask for path, or use path from config
 $path = "/data.txt"
 
-$use_config = $false # use config below. Config upper will be used anyway
-$server = "some.ip.or.domain"
-$user = "user"
-$passwd = "password" #password will use only ftp client, if enabled
+$use_config = $true # use config below. Config upper will be used anyway
+$server = "10.17.89.55"
+$user = "admin"
 
 #script
 
@@ -17,6 +16,10 @@ Remove-Item -path "$($ENV:Temp)\$($tmpFile1.Name)" -force
 
 $tmpFile2 = New-TemporaryFile
 Remove-Item -path "$($ENV:Temp)\$($tmpFile2.Name)" -force
+
+$tmpFile3 = New-TemporaryFile
+Remove-Item -path "$($ENV:Temp)\$($tmpFile3.Name)" -force
+$tmpFile3 = "$($tmpFile3.Name).csv"
 
 if ($use_ssh){
     if ($use_config){
@@ -38,11 +41,11 @@ else{
     else{
         $server = Read-Host -Prompt 'Enter IP of server'
         $user = Read-Host -Prompt 'Enter user on server'
-        $passwdSecured = Read-Host -AsSecureString -Prompt 'Enter password of user'
     }
     if ($ask_for_path){
         $path = Read-Host -Prompt 'Enter path to file on server'
     }
+    $passwdSecured = Read-Host -AsSecureString -Prompt 'Enter password of user'
     $client = New-Object System.Net.WebClient
     $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwdSecured)
     $passwd = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
@@ -50,18 +53,26 @@ else{
     $client.DownloadFile("ftp://$($server)/$($path)", "$($ENV:Temp)\$($tmpFile1.Name)")
 }
 
-Get-Content "$($ENV:Temp)\$($tmpFile1.Name)" | Select-Object -Skip 14 | Out-File "$($ENV:Temp)\$($tmpFile2.Name)"
+#delete header
+Get-Content "$($ENV:Temp)\$($tmpFile1.Name)" | Select-Object -Skip 12 | Out-File "$($ENV:Temp)\$($tmpFile2.Name)"
+
+#convert to csv
+#https://forum.uipath.com/t/how-to-use-power-shell-to-change-tab-to/9610/2
+(Get-Content "$($ENV:Temp)\$($tmpFile2.Name)") -replace “`t”, ";" | Set-Content "$($ENV:Temp)\$($tmpFile3)"
 
 # TMP files
 # $tmpFile1.Name = original downloaded file
 # $tmpFile2.Name = file without first 14 lines
+# $tmpFile3 = csv file
 
 if ($debug){
     echo "$($ENV:Temp)\$($tmpFile1.Name)"
     echo "$($ENV:Temp)\$($tmpFile2.Name)"
+    echo "$($ENV:Temp)\$($tmpFile3)"
 }
 
 if ($delete_tmp_files){
-    Remove-Item -path "$($ENV:Temp)\$($tmpFile1.Name)" -force    #rm temp file
-    Remove-Item -path "$($ENV:Temp)\$($tmpFile2.Name)" -force    #rm temp file
+    Remove-Item -path "$($ENV:Temp)\$($tmpFile1.Name)" -force
+    Remove-Item -path "$($ENV:Temp)\$($tmpFile2.Name)" -force
+    Remove-Item -path "$($ENV:Temp)\$($tmpFile3.Name)" -force
 }
